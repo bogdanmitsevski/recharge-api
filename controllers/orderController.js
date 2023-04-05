784754f88c9c53b2d43384d1c11fd366d91cf5c6
unknown <mitsevskyi.corporate@gmail.com>
2023-04-05 18:00:50 +0300

    feat: add discount type checking

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,15 +2,32 @@ const fetch = require('node-fetch');
 class OrderController {
     async GetOrderData(req, res) {
         try {
-            if (req.body.order.total_discounts !== 0) {
+            let myHeaders = new fetch.Headers();
+            myHeaders.append("Content-Type", "application/json");
+            myHeaders.append("X-Recharge-Access-Token", process.env.ACCESS_TOKEN);
+            myHeaders.append("X-Recharge-Version", "2021-11");
+            let requestDiscountOptions = {
+                method: 'GET',
+                headers: myHeaders,
+            };
+            if (req.body.order.discount_codes !== null) { // перевірка чи існує цей купон в recharge
+                const discountResponse = await fetch(`https://api.rechargeapps.com/discounts/${req.body.order.discount_codes[0].recharge_discount_id}`, requestDiscountOptions) // отримання списку знижок, які є в recharge
+                    .then(res => res.json())
+                    .catch(error => console.log('error', error));
+                if(discountResponse.discount.status !== 'disabled') { // перевірка чи існуючий купон є активним
+                    console.log('Скидка применена');                // купон сам додається, бек не запускаємо
+                    res.status(200).send();
+                }
+                else {                                       //якщо купон неактивний на стороні recharge, то знижка на стороні recharge не додається
+                    console.log('Скидка неактивна');
+                    res.status(200).send();
+                }
+            }
+            else if (req.body.order.discount_codes === null) { //якщо купона немає в recharge(script editor варіант), то запускаємо бек, який додає знижку відповідно до купона з script editora
                 const percentageDiscount = 1 - (req.body.order.total_discounts / req.body.order.total_line_items_price);
                 const dataArray = [];
                 for (let i = 0; i < (req.body.order.line_items).length; i++) {
                     dataArray.push({ subscriptionId: req.body.order.line_items[i].subscription_id, discountPrice: req.body.order.line_items[i].price * percentageDiscount });
-                    let myHeaders = new fetch.Headers();
-                    myHeaders.append("Content-Type", "application/json");
-                    myHeaders.append("X-Recharge-Access-Token", process.env.ACCESS_TOKEN);
-                    myHeaders.append("X-Recharge-Version", "2021-11");
                     let requestOptions = {
                         method: 'PUT',
                         headers: myHeaders,
